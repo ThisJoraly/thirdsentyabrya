eed83ee910639e2e4ca37c10dee72b78c2a950f2
Artyom Findin <tyomajoraly@gmail.com>
2024-04-30 19:36:47 +0300

    First commit

---
+++ Model/DayModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thirdsentyabrya.Model
+{
+    public class DayModel
+    {
+        public DateTime Date { get; set; }
+        public ObservableCollection<ItemModel> SelectedItems { get; set; }
+        public string IconPath { get; set; }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using thirdsentyabrya.Model;
+using thirdsentyabrya.Util;
+
+namespace thirdsentyabrya.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ObservableCollection<DayModel> _days;
+        public ObservableCollection<DayModel> Days
+        {
+            get => _days;
+            set
+            {
+                _days = value;
+            }
+        }
+
+        public ICommand PreviousMonthCommand { get; private set; }
+        public ICommand NextMonthCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            Days = new ObservableCollection<DayModel>();
+            PreviousMonthCommand = new RelayCommand(PreviousMonth);
+            NextMonthCommand = new RelayCommand(NextMonth);
+            LoadDays();
+        }
+
+        private void LoadDays()
+        {
+            // TODO сделать
+        }
+
+        private void PreviousMonth()
+        {
+            // TODO сделать
+        }
+
+        private void NextMonth()
+        {
+            // TODO сделать
+        }
+    }
+}
